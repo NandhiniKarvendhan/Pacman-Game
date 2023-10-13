40c38acd80dd98a74f19ccc7d4c2987094628647
NandhiniKarvendhan <nandhukannan10@gmail.com>
2023-10-13 15:50:40 +0530

    making ghost grey

--- game.js
+++ game.js
@@ -11,12 +11,26 @@ const initGame = () => {
   pacmanX += velocityX;
   pacmanY += velocityY;
   html = `<div class="pacman" style = "grid-area : ${pacmanY}/${pacmanX}"><img src="./img/PacMan1.png" alt="pacman-img"/></div>`;
-  html += `<div class="ghost" style = "grid-area : 1/12"><img src="./img/red_ghost.png" alt="ghost-img"/></div>`;
-  html += `<div class="ghost" style = "grid-area : 8/25"><img src="./img/pink_ghost.png" alt="ghost-img"/></div>`;
-  html += `<div class="ghost" style = "grid-area : 31/18"><img src="./img/green_ghost.png" alt="ghost-img"/></div>`;
-  html += `<div class="ghost" style = "grid-area : 24/9"><img src="./img/purple_ghost.png" alt="ghost-img"/></div>`;
-
+  html += `<div class="ghost" style = "grid-area : 1/12"><img class="ghost-img" src="./img/red_ghost.png" alt="ghost-img"/></div>`;
+  html += `<div class="ghost" style = "grid-area : 8/25"><img class="ghost-img" src="./img/pink_ghost.png" alt="ghost-img"/></div>`;
+  html += `<div class="ghost" style = "grid-area : 31/18"><img class="ghost-img" src="./img/green_ghost.png" alt="ghost-img"/></div>`;
+  html += `<div class="ghost" style = "grid-area : 24/9"><img class="ghost-img" src="./img/purple_ghost.png" alt="ghost-img"/></div>`;
+  html += `<div class="power-pellet" style="grid-area: 4/20"> <img src="img/pallet.png" /> </div>
+<div class="power-pellet" style="grid-area: 19/16"> <img src="img/pallet.png" /></div>
+<div class="power-pellet" style="grid-area: 2/3"> <img src="img/pallet.png" /></div>
+<div class="power-pellet" style="grid-area: 9/15"> <img src="img/pallet.png" /></div>
+<div class="power-pellet" style="grid-area: 18/22"> <img src="img/pallet.png" /></div>
+<div class="power-pellet" style="grid-area: 19/6"> <img src="img/pallet.png" /></div>
+<div class="power-pellet" style="grid-area: 12/20"> <img src="img/pallet.png" /></div>`;
   gameBoard.innerHTML = html;
+
+  const ghostImage = document.querySelectorAll("#game-board .ghost-img");
+
+  if ((pacmanY == 2) & (pacmanX == 3)) {
+    for (let i = 0; i < ghostImage.length; i++) {
+      ghostImage[i].style.opacity = "0";
+    }
+  }
 };
 
 const pacmanDirection = (e) => {
@@ -38,4 +52,4 @@ const pacmanDirection = (e) => {
 };
 // To move pacman
 document.addEventListener("keydown", pacmanDirection);
-setInterval(initGame, 150);
+setInterval(initGame, 250);
